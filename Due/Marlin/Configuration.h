537cbb88c144aaa896121c8b02fa81e581acb0bf
domonoky <domonoky@2printbeta.de>
2015-06-17 18:57:28 +0200

    Set Bed min Temperature to 0.

--- Due/Marlin/Configuration.h
+++ Due/Marlin/Configuration.h
@@ -163,7 +163,7 @@ Here are some standard links for getting your machine calibrated:
 #define HEATER_1_MINTEMP 5
 #define HEATER_2_MINTEMP 5
 #define HEATER_3_MINTEMP 5
-#define BED_MINTEMP 5
+#define BED_MINTEMP 0
 
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
